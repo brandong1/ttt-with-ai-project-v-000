806cc65d62872b33b631c99c0df176f10efd81d4
Brandon Green <brandon@brandonleeproductions.com>
2018-07-13 17:41:10 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,10 +2,10 @@ class Player::Human < Player
   module Players
     class Human < Player
 
-  def move(board)
-    puts "Please enter 1-9:"
-    gets.strip
+      def move(board)
+        puts "Please enter 1-9:"
+        gets.strip
+      end
+    end
   end
-  end
-end
 end
