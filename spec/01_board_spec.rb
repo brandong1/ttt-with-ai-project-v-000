6568420efa8e756df5a7eaaaaaa5f89645f79db0
Brandon Green <brandon@brandonleeproductions.com>
2018-07-13 18:25:47 +0000

    automatically backed up by learn

--- spec/01_board_spec.rb
+++ spec/01_board_spec.rb
@@ -30,7 +30,7 @@ describe "Board" do
   describe '#initialize' do
     it 'sets the cells of the board to a 9 element array of " "' do
       board = Board.new
-      binding.pry
+
       expect(board.cells).to match_array(Array.new(9, " "))
     end
   end
