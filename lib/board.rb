3476c486dfbd9088b0621f07ab28302b96441613
Brandon Green <brandon@brandonleeproductions.com>
2018-07-13 17:57:16 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -6,7 +6,7 @@ class Board
 
 
     # @cells = Array.new(9, " ") # This is duplicate code in the reset! method
-    # reset! # Calling this doesn't pass the test
+    reset! # Calling this doesn't pass the test
 
   end
 
