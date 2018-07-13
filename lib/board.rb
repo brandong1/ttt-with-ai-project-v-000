d4242a38c2ca4408a6323b65599819f0817a366b
Brandon Green <brandon@brandonleeproductions.com>
2018-07-13 18:32:44 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -16,6 +16,61 @@ require 'pry'
 #      #binding.pry
 #
 #   end
+# class Board
+#   attr_accessor :cells
+#
+#   def initialize()
+#     reset!
+#   end
+#
+#   def reset!
+#     @cells = Array.new(9, " ")
+#   end
+#
+#
+#
+#   def display
+#     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+#     puts "-----------"
+#     puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+#     puts "-----------"
+#     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+#   end
+#
+#   def full?
+#     cells.all?{|token| token == "X" || token == "O"}
+#   end
+#
+#   # def full?
+#   #   !@board.include?(" ")
+#   # end
+#
+#   def turn_count
+#     cells.count{|token| token == "X" || token == "O"}
+#   end
+#
+#   def valid_move?(space)
+#       (0..8).include?(space) && space_available?(space)
+#   end
+#   # def valid_move?(input)
+#   #   input.to_i.between?(1,9) && !taken?(input)
+#   # end
+#
+#   def update(input, player)
+#     cells[input.to_i] = player.token
+#   end
+#
+#   def position(input)
+#     cells[input.to_i]
+#   end
+#
+#   def taken?(input)
+#     !(position(input) == " " || position(input) == "")
+#   end
+#
+#
+# end
+# Board.new
 class Board
   attr_accessor :cells
 
@@ -26,8 +81,6 @@ class Board
   def reset!
     @cells = Array.new(9, " ")
   end
-
-
 
   def display
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
@@ -41,33 +94,23 @@ class Board
     cells.all?{|token| token == "X" || token == "O"}
   end
 
-  # def full?
-  #   !@board.include?(" ")
-  # end
-
   def turn_count
     cells.count{|token| token == "X" || token == "O"}
   end
 
-  def valid_move?(space)
-      (0..8).include?(space) && space_available?(space)
+  def valid_move?(input)
+    input.to_i.between?(1,9) && !taken?(input)
   end
-  # def valid_move?(input)
-  #   input.to_i.between?(1,9) && !taken?(input)
-  # end
 
   def update(input, player)
-    cells[input.to_i] = player.token
+    cells[input.to_i-1] = player.token
   end
 
   def position(input)
-    cells[input.to_i]
+    cells[input.to_i-1]
   end
 
   def taken?(input)
     !(position(input) == " " || position(input) == "")
   end
-
-
 end
-Board.new
