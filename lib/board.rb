2d1d73d76e227c9991d0c43ebff90ab8e2edbfc4
Brandon Green <brandon@brandonleeproductions.com>
2018-07-13 18:02:06 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -4,9 +4,8 @@ class Board
 
   def initialize
 
-    cells
-    # @cells = Array.new(9, " ") # This is duplicate code in the reset! method
-    #reset! # Calling this doesn't pass the test
+  # @cells = Array.new(9, " ") # This is duplicate code in the reset! method
+  reset! # Calling this doesn't pass the test
 
   end
 
